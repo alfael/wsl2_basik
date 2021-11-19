716b2cee951bbabf2c29e1bcf36c86f9df4d53df
alfael <alfael.sc@gmail.com>
2021-11-19 15:06:46 +0100

    Import build script

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 cd $(dirname $0)
-#export PATH="/opt/gcc-latest/bin:$PATH"
-#export CROSS_COMPILE=x86_64-pc-linux-gnu-
 export KCONFIG_CONFIG=Microsoft/config-basik
 export ARCH=x86_64
 export SUBARCH=x86_64
 export KBUILD_CFLAGS="-Wno-maybe-uninitialized -Wno-memset-elt-size -Wno-duplicate-decl-specifier"
-#nice make  clean
-#nice make  mrproper
-cp -rf arch/x86/boot/bzImage /mnt/c/wsl2/basik
+nice make  clean
+nice make  mrproper
 nice make -j$(nproc --all) 2>&1 | tee build.log
+cp -rf arch/x86/boot/bzImage /mnt/c/wsl2/basik
